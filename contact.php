ac20f921c561ba3ef9cdee4ee3852974938ba978
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-04-26 12:07:41 -0400

    playing with contact form

--- contact.php
+++ contact.php
@@ -2,13 +2,15 @@
 if($_POST["submit"]) {
     $recipient="ishaniamin@gmail.com"; //Enter your mail address
     $subject="Contact from Website"; //Subject 
+
     $firstName=$_POST["firstName"];
     $lastName=$_POST["lastName"];
     $senderEmail=$_POST["email"];
     $message=$_POST["message"];
+
     $mailBody="First Name: $firstName\nLast Name: $lastName\nEmail Address: $senderEmail\n\nMessage: $message";
-    mail($recipient, $subject, $mailBody);
-	sleep(1);
-	header("http://ishaniamin.herokuapp.com/"); // Set here redirect page or destination page
+    mail($recipient, $subject, $mailBody), "From: $sender <$senderEmail>");;
+	
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
 ?>
