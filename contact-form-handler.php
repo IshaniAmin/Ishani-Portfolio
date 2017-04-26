54333ed4f14d8604d04ae1f45889d5076d85ef9c
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-04-26 11:56:51 -0400

    deleted images

---
+++ contact-form-handler.php
@@ -0,0 +1,14 @@
+<?php
+if($_POST["submit"]) {
+    $recipient="ishaniamin@gmail.com"; //Enter your mail address
+    $subject="Contact from Website"; //Subject 
+    $firstName=$_POST["firstName"];
+    $lastName=$_POST["lastName"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["comments"];
+    $mailBody="First Name: $firstName\nLast Name: $lastName\nEmail Address: $senderEmail\n\nMessage: $message";
+    mail($recipient, $subject, $mailBody);
+	sleep(1);
+	header("Location:www.google.com"); // Set here redirect page or destination page
+}
+?>
